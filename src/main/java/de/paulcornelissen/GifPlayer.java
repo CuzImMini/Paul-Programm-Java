7176f8afe09835dd4cee8b0914e5a3b2f060d363
Paul Cornelissen <cornelissen2005@gmail.com>
2022-09-08 10:52:30 +0200

    Gif-animation hinzugefügt

---
+++ src/main/java/de/paulcornelissen/GifPlayer.java
@@ -0,0 +1,23 @@
+package de.paulcornelissen;
+
+import basis.Hilfe;
+
+import java.io.IOException;
+
+public class GifPlayer {
+
+    public GifPlayer() {
+    }
+
+    public void playHorseGif(Instance instance) throws IOException {
+        while (true) {
+            for (int i = 0; i < 15; i++) {
+
+                String currentPath = new java.io.File("./repo/de/wikipedia/horse-" + i + ".png").getCanonicalPath();
+                instance.setBackgroundPhoto(currentPath);
+                Hilfe.warte(100);
+            }
+        }
+    }
+
+}
--- src/main/java/de/paulcornelissen/Main.java
+++ src/main/java/de/paulcornelissen/Main.java
@@ -1,23 +1,24 @@
 package de.paulcornelissen;
 
-import basis.Hilfe;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] s) {
+    public static void main(String[] s) throws IOException {
 
         //Starte Paint
-        Application paulPaint = new Application("Paul-Paint");
+        //Application paulPaint = new Application("Paul-Paint");
 
         //Testinstanz
-        //Instance instance = new Instance("Bildergalerie", 500,500,true);
+        Instance instance = new Instance("Bildergalerie", 600, 400, false);
+        new GifPlayer().playHorseGif(instance);
 
         //Test-Bildergalerie
         //TODO Bildergalerie in Gui implementieren
         // - Knopf in GUI hinzufügen
         //   - Knopf ruft über öffentliche Variablen die Klasse
         //     Bildergalerie in der laufenden Instanz auf
-        Bildergalerie bg = new Bildergalerie();
+        //Bildergalerie bg = new Bildergalerie();
     }
 
 }
